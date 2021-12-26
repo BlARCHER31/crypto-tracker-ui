c9117200c75e2778754e9fc4735431bb2d31b95e
Blake Archer <blarcher31@gmail.com>
2021-12-26 15:38:39 -0600

    changed to dark colors

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -47,7 +47,7 @@ class Form extends Component {
 
   renderButton(label) {
     return (
-      <button disabled={this.validate()} className='btn btn-primary'>
+      <button disabled={this.validate()} className='btn btn-dark m-2'>
         {label}
       </button>
     )
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const NavBar = ({ user }) => {
   return (
-    <nav className='navbar navbar-expand-md navbar-dark bg-primary'>
+    <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
       <div className='container-fluid'>
         <Link className='navbar-brand' to='/'>
           Crypto Portfolio Tracker
