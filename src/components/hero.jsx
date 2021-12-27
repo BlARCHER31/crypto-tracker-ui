23d3c8ac99eef7771d1a0b5d02d5e96f73abe501
Blake Archer <blarcher31@gmail.com>
2021-12-27 17:34:41 -0600

    added hero component

---
+++ src/components/hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import image from '../images/archer-logo.svg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='row justify-content-md-center title'>
+        <div className='col-6 align-self-center'>
+          <h1 className='hero-title'>Crypto Portfolio Tracker</h1>
+        </div>
+        <div className='col-md-auto title'>
+          <h2>Archer Programming</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
